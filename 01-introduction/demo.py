d5bb6e5dfb9c8498997da10a45e8907e4adbaea6
xiedongmingming <986617424@qq.com>
2023-07-03 14:42:19 +0800

    优化

--- 01-introduction/demo.py
+++ 01-introduction/demo.py
@@ -1,6 +1,8 @@
-# 导入gradio
+# 导入GRADIO
 import gradio as gr
-# 导入transformers相关包
+
+# 导入TRANSFORMERS相关包
 from transformers import *
-# 通过Interface加载pipeline并启动阅读理解服务
+
+# 通过INTERFACE加载PIPELINE并启动阅读理解服务
 gr.Interface.from_pipeline(pipeline("question-answering", model="uer/roberta-base-chinese-extractive-qa")).launch()
